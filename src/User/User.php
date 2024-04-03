f1ac684a7d4c61ddae961fe82d5db8c1fe2fd324
Cristiano Ribeiro <christiribeiro2011@gmail.com>
2024-04-03 02:24:36 -0300

    obter o maior departamento de cada usuário

--- src/User/User.php
+++ src/User/User.php
@@ -17,6 +17,35 @@ class User {
         return $usernames;
     }
 
+	public function getMaxDepartmentForEachUser() {
+        // Consulta SQL para obter o maior departamento de cada usuário
+        $query = 'SELECT ud.user, MAX(d.employees) AS max_employees, d.name AS department_name
+                  FROM user_department ud
+                  JOIN department d ON ud.department = d.id
+                  GROUP BY ud.user';
+
+        $results = $this->db->q($query);
+
+        $maxDepartments = [];
+
+        // Iterar sobre os resultados
+        foreach ($results as $result) {
+            $userId = $result['user'];
+            $maxEmployees = $result['max_employees'];
+            $departmentName = $result['department_name'];
+
+            // Armazenar o maior departamento para cada usuário no array
+            $maxDepartments[$userId] = [
+                'max_employees' => $maxEmployees,
+                'department_name' => $departmentName
+            ];
+		}
+
+		return $maxDepartments;
+
+	}
+
+
 	public function setDb($db) {
 		if (!$db || $db->isClosed()) {
 			return false;
